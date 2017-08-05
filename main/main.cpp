1e4290cafae286631d2dc674294f8c8702f08d96
Payton R. White <payton@prehiti.net>
2017-08-05 12:56:43 -0700

    Minor tweaks to sdkconfig (wifi doesn't start up if broadcast/mcast ping is on), tasks (set all builds to be parallel through MAKEFLAGS), main.cpp (changed LED blink pin and rate).

--- main/main.cpp
+++ main/main.cpp
@@ -41,11 +41,11 @@ extern "C" void app_main(void)
     tcpip_adapter_init();
     init_wifi();
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_NUM_5, GPIO_MODE_OUTPUT);
     int level = 0;
     while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
+        gpio_set_level(GPIO_NUM_5, level);
         level = !level;
-        vTaskDelay(300 / portTICK_PERIOD_MS);
+        vTaskDelay(3000 / portTICK_PERIOD_MS);
     }
 }
